08d9d102fcfe01c919395dd0a5e1d238ac6dc1b8
jaimevale <jaimev@vc-soft.com>
2021-11-27 10:41:26 -0500

    version inicial

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -1,7 +1,7 @@
 package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+import java.math.BigInteger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,4 +16,17 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+	
+	@GetMapping("/factorial")
+	public BigInteger factorial(@RequestParam(value="value") int value) {
+		return fact(value);
+	}
+ 
+ 
+	BigInteger fact(int n){    
+	 if (n == 0)    
+		 return BigInteger.valueOf(1);    
+	 else    
+		 return(fact(n-1).multiply(BigInteger.valueOf(n)));    
+	 } 
 }
