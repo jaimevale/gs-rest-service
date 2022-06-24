c4a4597411d14c5232956520eecc3cc3e36fa7bb
jaimev <jaimev@vc-soft.com>
2022-06-23 19:06:00 -0500

    cambio

--- complete/src/main/java/com/example/restservice/AccountController.java
+++ complete/src/main/java/com/example/restservice/AccountController.java
@@ -11,37 +11,37 @@ import java.util.List;
 @RestController
 public class AccountController {
 
-
-    @GetMapping("/cuenta-multiple")
-    public String[] cuentas() {
-        return cuentasRepetidas();
-    }
-
-    /**
-     * Solo interesan las cuentas que aparezcan repetidas
-     * @return
-     */
-    private String[] cuentasRepetidas() {
-        // entregar solo las cuentas repetidas
-        //  Evitar soluciones con complejidad O(n^2)
-        var cuentas = listaCuentas();
-        String[] repetidas = new String[cuentas.size()];
-
-        for (int i = 0; i < cuentas.size(); i++) {
-            for (int j = 1; j < cuentas.size(); j++) {
-                if(cuentas.get(i) == cuentas.get(j)){
-                    repetidas[i] = cuentas.get(i);
-                }
-            }
-        }
-        return repetidas;
-    }
-
-
-    private List<String> listaCuentas(){
-        String[] unEjemplo = new String[] { "ahorros", "credito", "hipotecaria", "afp", "ahorros", "vis",
-                "ahorros", "kids", "amparada", "afp", "blue", "vista", "blue"
-        };
-        return Arrays.asList(unEjemplo);
-    }
+@GetMapping("/cuentas-unicas")
+   public String[] cuentasUnicas() {
+       return cuentasNoRepetidas();
+   }
+ 
+   /**
+    * Solo interesan las cuentas que aparezcan NO repetidas
+    * @return
+    */
+   private String[] cuentasNoRepetidas() {
+       // entregar solo las cuentas NO repetidas
+       //  Evitar soluciones con complejidad O(n^2)
+       var cuentas = listaCuentas();
+       String[] repetidas = new String[cuentas.size()];
+ 
+       for (int i = 0; i < cuentas.size(); i++) {
+           for (int j = 1; j < cuentas.size(); j++) {
+               if(cuentas.get(i) == cuentas.get(j)){
+                   repetidas[i] = cuentas.get(i);
+               }
+           }
+       }
+       return repetidas;
+   }
+ 
+ 
+   private List<String> listaCuentas(){
+       String[] unEjemplo = new String[] { 
+               "ahorros", "credito", "hipotecaria", "afp", "ahorros", "vis",
+               "ahorros", "kids", "amparada", "afp", "blue", "vista", "blue"
+       };
+       return Arrays.asList(unEjemplo);
+   }
 }
