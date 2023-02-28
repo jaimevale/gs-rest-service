1a07543a961ebde1789474668aec00b4c1d16e93
jaimev <jaimev@vc-soft.com>
2023-02-28 11:35:34 -0500

    repeticiones

--- complete/src/main/java/com/example/restservice/AltController.java
+++ complete/src/main/java/com/example/restservice/AltController.java
@@ -4,17 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class AltController {
     @GetMapping("/acumulador")
     public int[] acumuladorController(
             @RequestParam(value="valores", defaultValue = "5,2,3,2,3,3,3,1")  int[] values){
-
         return values;
-
-
     }
 
+
+    @GetMapping("/repeticiones")
+    public Map.Entry<Integer, Integer> repeticiones(
+            @RequestParam(value="valores", defaultValue = "5,5,1,2,2,3,4,5,6,6,6,6")  int[] values){
+        return Map.entry(6,4);
+    }
 
 
 
